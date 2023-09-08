72512134335dfc8ae152cc1953e1902c39a09c94
NigarValikanova <minayaff@code.edu.az>
2023-09-08 16:53:30 +0400

    14 lesson

---
+++ Lesson-14/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string action = "program basladi";
+        ConsoleLog consoleLog = new ConsoleLog();
+        consoleLog.WriteMethod(action);
+
+        FileLog fileLog = new FileLog();
+        fileLog.WriteMethod(action);
+
+    }
+}
+
+#region Ders izah
+interface Mother
+{
+    string Name { get; set; }
+    int Test();
+}
+interface Father
+{
+    int Test2();
+}
+class Child : Mother, Father
+{
+    public string Name { get; set; }
+
+    public int Test()
+    {
+        throw new NotImplementedException();
+    }
+    public int Test2()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+#endregion
+
+interface ILog
+{
+    void WriteMethod(string data);
+}
+class ConsoleLog : ILog
+{
+    public void WriteMethod(string data)
+    {
+       string date= DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss") + data;
+        Console.WriteLine(date);
+    }
+}
+
+class FileLog : ILog
+{
+    public void WriteMethod(string data)
+    {
+        string date = DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss") + data;
+
+        using (var file = File.AppendText(@"C:\Users\Minaye.Ferzeliyeva\source\repos\FirstApp\bin\Debug\log.txt"))
+        {
+            file.WriteLine(date);
+        }
+
+    }
+}
