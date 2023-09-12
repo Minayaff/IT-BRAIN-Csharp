6e805068879bc73d47e93e70fe4fd0afe0268daa
NigarValikanova <minayaff@code.edu.az>
2023-09-12 09:41:59 +0400

    lesson-15

---
+++ Lesson-15/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        //FileStream file = new FileStream("C:\\test.txt", FileMode.Append, FileAccess.Write);
+        //using (StreamWriter streamWriter=new StreamWriter(file))
+        //{
+        //    string data = "sagol";
+        //    streamWriter.WriteLine(data);
+        //}
+
+        FileStream file = new FileStream("C:\\test.txt", FileMode.Open, FileAccess.Read);
+        StreamReader streamReader = new StreamReader(file);
+        streamReader.BaseStream.Seek(0, SeekOrigin.Begin);
+         string data=streamReader.ReadToEnd();
+        Console.WriteLine(data);
+
+    }
+
+}
+
